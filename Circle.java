8c79dabc04349ecc2e7ba594490a026b5f30eade
lynnkays <95385645+lynnkays@users.noreply.github.com>
2022-01-06 18:28:13 +0000

    Initial commit

---
+++ Circle.java
@@ -0,0 +1,52 @@
+/*******************************************************************
+* <Kaylynn Diaz-Schott>
+*
+*<Adds characteristics of a circle to an Array of objects and compares another object
+*with objects in the array>
+*******************************************************************/
+public class Circle {
+	public String color = "blue";
+	public double radius = 1.0;
+	public final double PI = 3.14159;
+	
+	Circle() {}
+	
+	public Circle(double newRadius, String newColor){
+		color = newColor;
+		radius = newRadius;	
+	}
+	
+	public String getColor(){
+		return color;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	public void setColor(String newColor){
+		color = newColor;	
+	}
+
+	public void setRadius(double newRadius){
+		radius = newRadius;
+	}
+	public double findArea () {
+		double area = (getRadius()* getRadius())* PI;
+		return area;
+	}
+	public String toString() {
+    return "\nColor: " + this.color + "\nRadius: " + this.radius + "\nArea: " +
+    Double.toString(findArea());
+	}
+	
+	public boolean equals (Circle c2){	
+		if (color.equals(c2.color)&& radius == c2.radius){
+			return true;
+		} else {
+			return false;
+		}
+	
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.text.DecimalFormat;
+
+public class Main {
+	public static void main (String[] args){
+		Scanner s = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("###,###.0");
+		final int MAX = 2;
+		Circle [] circleArray = new Circle[3];
+
+    for(int i = 0; i < circleArray.length; i++){
+      System.out.print("Enter a color: ");
+      String color = s.nextLine();
+
+      System.out.print("Enter the radius: ");
+		  double radius = Double.parseDouble(s.nextLine());
+      circleArray[i] = new Circle(radius, color);
+    }
+
+    for (int i = 0; i < circleArray.length; i++){
+      System.out.print(circleArray[i].toString());
+    }
+		
+		System.out.print("\nCheck if Circle is in List\nEnter in Color for another Circle: ");
+		String color = s.nextLine();
+		System.out.print("Enter in the Radius for another Circle: ");
+		double radius = Double.parseDouble(s.nextLine());
+		
+		Circle checkCircle = new Circle(radius,color);
+		
+		for (int i = 0; i < circleArray.length; i++){
+			if (circleArray[i].equals(checkCircle)== true){
+				System.out.println("Entered circle equals a created object in the array");
+				break;
+			}else {
+				System.out.println("Entered circle does NOT equal a created object in the array");
+				break;
+			}
+		}	
+	}
+}
